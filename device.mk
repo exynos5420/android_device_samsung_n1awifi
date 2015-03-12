eafe2ede5e830176c29fa60421ffa137057c1c41
ShevT <shev_t@mail.ru>
2015-03-12 21:43:51 +0300

    keylayouts: move from vendor to device tree

--- device.mk
+++ device.mk
@@ -73,7 +73,8 @@ PRODUCT_PACKAGES += \
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl
 
 # Keystore
 PRODUCT_PACKAGES += \
