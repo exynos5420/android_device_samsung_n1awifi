d7f58e9e484a5476895d295423b86ef4886d34ad
ShevT <shev_t@mail.ru>
2015-04-06 11:17:27 +0300

    use system hwui memory config

--- device.mk
+++ device.mk
@@ -221,5 +221,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 
+# call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/samsung/lt033g/lt033g-vendor.mk)
