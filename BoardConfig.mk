aeadd37ca9882a5670c4520eefd5020e5f00c56a
ShevT <shev_t@mail.ru>
2015-04-26 09:56:12 +0300

    BoardConfig: added WIFI_BAND

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,6 +198,7 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_BAND                        := 802_11_ABG
 
 # inherit from the proprietary version
 -include vendor/samsung/lt033g/BoardConfigVendor.mk
