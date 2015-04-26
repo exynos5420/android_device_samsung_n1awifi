992fd03b5a042957f45d39fccde1b53832e2d76d
ShevT <shev_t@mail.ru>
2015-04-26 10:00:56 +0300

    BoardConfig: Enable dex-preoptimization to speed up first boot sequence

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,6 +145,9 @@ BOARD_FLASH_BLOCK_SIZE := 4096
 # PowerHAL
 TARGET_POWERHAL_VARIANT := universal5420
 
+# Enable dex-preoptimization to speed up first boot sequence
+WITH_DEXPREOPT := true
+
 # UMS
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
 
