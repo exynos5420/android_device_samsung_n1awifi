08613bdb49746abaa0c12bb9c482bf930114790a
ShevT <shev_t@mail.ru>
2015-04-26 09:58:12 +0300

    BoardConfig: vendor must first perform

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,9 @@
 
 LOCAL_PATH := device/samsung/lt033g
 
+# inherit from the proprietary version
+-include vendor/samsung/lt033g/BoardConfigVendor.mk
+
 # Platform
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
@@ -199,6 +202,3 @@ WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BAND                        := 802_11_ABG
-
-# inherit from the proprietary version
--include vendor/samsung/lt033g/BoardConfigVendor.mk
