beea44e9c29052e1a1a8c397b3aa78ae80df5053
ShevT <shev_t@mail.ru>
2015-06-08 18:38:21 +0300

    lt033g: remove audio_effects.conf

--- device.mk
+++ device.mk
@@ -35,7 +35,6 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Boot animation
