b2b0feac6b23c931c4b12d7910e061e2c5e8249f
ShevT <shev_t@mail.ru>
2015-11-06 22:00:13 +0300

    wifi: remove wpa_supplicant.conf

--- device.mk
+++ device.mk
@@ -195,7 +195,6 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
