5034b7fade6e799b6bbdb7a745008f81a3573d0b
ShevT <shev_t@mail.ru>
2015-11-08 15:59:40 +0300

    lt033g: added hwui settings from P601SERDOJ2

--- device.mk
+++ device.mk
@@ -257,8 +257,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m \
 
-# call hwui memory config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+# HWUI CACHES
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=96 \
+    ro.hwui.layer_cache_size=64 \
+    ro.hwui.path_cache_size=16 \
+    ro.hwui.shape_cache_size=4 \
+    ro.hwui.gradient_cache_size=2 \
+    ro.hwui.drop_shadow_cache_size=8 \
+    ro.hwui.r_buffer_cache_size=4 \
+    ro.hwui.text_small_cache_width=2048 \
+    ro.hwui.text_small_cache_height=2048 \
+    ro.hwui.text_large_cache_width=4096 \
+    ro.hwui.text_large_cache_height=4096 \
+    ro.hwui.fbo_cache_size=16
 
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/samsung/lt033g/lt033g-vendor.mk)
