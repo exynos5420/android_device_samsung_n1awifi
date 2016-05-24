7f819513825129b2e11e57e5fd70e1378c6cb259
ShevT <shev_t@mail.ru>
2016-05-24 13:48:30 +0300

    Revert "lt033g: remove samsung symbols lib"
    This reverts commit d864000966f01532d2a5414b691b221773a8239c.

--- device.mk
+++ device.mk
@@ -66,6 +66,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1
 
+PRODUCT_PACKAGES += \
+    libsamsung_symbols
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.universal5420 \
