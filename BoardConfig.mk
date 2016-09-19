18d2b6b52309cfd89f25c98c4d6d8ca0514c949f
Paul Keith <javelinanddart@gmail.com>
2016-09-19 16:10:09 -0700

    consumerir: Cleanup HAL
    * Unify HAL
    * Take some styling changes from JF, thanks to @zwliew
    
    Change-Id: I9e5160161ec8d942e4fa22712c00e0dec66bd365
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,9 @@ BOARD_USES_SCALER := true
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
+# IR Blaster
+IR_HAS_ONE_FREQ_RANGE := true
+
 # Hardware
 BOARD_HARDWARE_CLASS += device/samsung/n1awifi/cmhw
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
