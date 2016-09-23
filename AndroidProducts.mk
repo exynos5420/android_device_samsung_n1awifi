0d0c21d21537d6596d7c16e3a5063365b74b5660
Joshndroid <fmxjoshiewah@gmail.com>
2016-09-23 16:35:04 +1000

    n1awifi : AOSP-ify device tree
    Change-Id: I0e69d414c0ad59d0fedea7e2841cfddc40cfdf70

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2013 The CyanogenMod Project
+#
+# Copyright 2015 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,12 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# Inherit from n1awifi device
-$(call inherit-product, device/samsung/n1awifi/device.mk)
-
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := full_n1awifi
-PRODUCT_DEVICE := n1awifi
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := AOSP on SM-P600
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_n1awifi.mk
--- aosp_n1awifi.mk
+++ aosp_n1awifi.mk
@@ -1,14 +1,14 @@
 #Inherit device configuration
 $(call inherit-product, device/samsung/n1awifi/full_n1awifi.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-PRODUCT_NAME := cm_n1awifi
+PRODUCT_NAME := aosp_n1awifi
 PRODUCT_DEVICE := n1awifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-P600 \
     PRODUCT_NAME=n1awifi \
     PRODUCT_DEVICE=n1awifi \
-    TARGET_DEVICE=n1awifi
+	TARGET_DEVICE=n1awifi
