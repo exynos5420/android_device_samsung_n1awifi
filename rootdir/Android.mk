771910d7371bd2837bb5381aee29218e2f8e7ac6
zakooz <marcotribosklol@gmail.com>
2016-10-08 01:18:20 +0100

    init: create init.universal5420.power.rc
    The key change here: moved from on boot to post-fs-data
    Original commit: https://github.com/exynos5420/android_device_samsung_klimtwifi/commit/97ecefd7fbf4c86c7fbdf9aa06fc7ccaa51e6a40
    Change-Id: I3ea8528a3ed9e5eb413d77eac98b4cd2182866a2

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -25,6 +25,14 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.universal5420.power.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= BOOT
+LOCAL_SRC_FILES		:= etc/init.universal5420.power.rc
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.universal5420.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
@@ -39,6 +47,8 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.samsung.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+ 
+
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.universal5420.rc
