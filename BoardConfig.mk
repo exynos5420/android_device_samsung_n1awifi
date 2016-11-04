c40bbb850ae5c65868dece8f1617b40a62cc52db
Sean Hoyt <seanhoyt963@gmail.com>
2016-11-03 22:07:11 -0500

    Use Exynos5420-common
    Change-Id: I219d8bd6062fd97c3e428313c46b48298bcfabde

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,132 +16,24 @@
 
 LOCAL_PATH := device/samsung/n1awifi
 
-# inherit from the proprietary version
--include vendor/samsung/n1awifi/BoardConfigVendor.mk
-
 # Platform
-BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
-TARGET_SLSI_VARIANT := cm
 TARGET_SOC := exynos5420
 
-# Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a15
-
-# Bionic Tuning
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
-
-# Bionic Tuning
-ARCH_ARM_USE_MEMCPY_ALIGNMENT := true
-BOARD_MEMCPY_ALIGNMENT := 64
-BOARD_MEMCPY_ALIGN_BOUND := 32768
-
 # Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
 # Bootloader
-TARGET_OTA_ASSERT_DEVICE := lt033g,lt03wifi,lt03wifiue,n1awifi
-
-# Camera
-BOARD_CAMERA_SNUMINTS := 34
-BOARD_NEEDS_MEMORYHEAPION := true
-#BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21 := true
-COMMON_GLOBAL_CFLAGS += -DCAMERA_SNUMINTS=$(BOARD_CAMERA_SNUMINTS)
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
-
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
+TARGET_OTA_ASSERT_DEVICE := lt03wifi,lt03wifiue,n1awifi
 
 # Kernel
-BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_CONFIG := cyanogenmod_deathly_n1awifi_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/exynos5420
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-
-# Charger/Healthd
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
-
-# Build the platform with Clang
-USE_CLANG_PLATFORM_BUILD := true
-
-# We use our lights hal
-TARGET_PROVIDES_LIBLIGHT := true
-
-# Bootloader
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-TARGET_BOOTLOADER_BOARD_NAME := universal5420
-
-# FIMG2D
-BOARD_USES_SKIA_FIMGAPI := true
-
-# GSC
-BOARD_USES_ONLY_GSC0_GSC1 := true
-
-# Graphics
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-
-# Exynos display
-BOARD_USES_VIRTUAL_DISPLAY := true
-
-# HWCServices
-BOARD_USES_HWC_SERVICES := true
-
-# SCALER
-BOARD_USES_SCALER := true
-
-# HDMI
-BOARD_USES_GSC_VIDEO := true
-BOARD_USES_CEC := true
-
-# Include path
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # IR Blaster
 IR_HAS_ONE_FREQ_RANGE := true
 
 # Hardware
-BOARD_HARDWARE_CLASS += device/samsung/n1awifi/cmhw
-
-# Samsung LSI OpenMAX
-COMMON_GLOBAL_CFLAGS += -DUSE_NATIVE_SEC_NV12TILED # use format from fw/native
-COMMON_GLOBAL_CFLAGS += -DWIDEVINE_PLUGIN_PRE_NOTIFY_ERROR
-
-# Samsung OpenMAX Video
-BOARD_USE_STOREMETADATA := true
-BOARD_USE_METADATABUFFERTYPE := true
-BOARD_USE_DMA_BUF := true
-BOARD_USE_ANB_OUTBUF_SHARE := true
-BOARD_USE_IMPROVED_BUFFER := true
-BOARD_USE_NON_CACHED_GRAPHICBUFFER := true
-BOARD_USE_GSC_RGB_ENCODER := true
-BOARD_USE_CSC_HW := true
-BOARD_USE_QOS_CTRL := false
-BOARD_USE_S3D_SUPPORT := true
-BOARD_USE_VP8ENC_SUPPORT := true
-
-# HEVC support in libvideocodec
-BOARD_USE_HEVC_HWIP := true
-BOARD_USE_HEVCDEC_SUPPORT := true
-
-# Sensors
-TARGET_NO_SENSOR_PERMISSION_CHECK := true
-
-# Samsung Seiren audio
-BOARD_USE_ALP_AUDIO := true
-BOARD_USE_SEIREN_AUDIO := true
+BOARD_HARDWARE_CLASS += $(LOCAL_PATH)/cmhw
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
@@ -153,61 +45,8 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2524971008
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12863913984
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Disable journaling on system.img to save space
-BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
-
-# Enable dex-preoptimization to speed up first boot sequence
-WITH_DEXPREOPT := true
-
-# UMS
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
-
-# Use these flags if the board has a ext4 partition larger than 2gb
-BOARD_HAS_LARGE_FILESYSTEM := true
-
-# Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.universal5420
-
-# SurfaceFlinger
-BOARD_USES_SYNC_MODE_FOR_MEDIA := true
-
-# Webkit
-ENABLE_WEBGL := true
-
-# WFD
-BOARD_USES_WFD := true
-
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
-BOARD_ANT_WIRELESS_POWER := "bluedroid"
-
-# Keymaster
-BOARD_USES_TRUST_KEYMASTER := true
-
-# Wifi
-BOARD_HAVE_SAMSUNG_WIFI          := true
-BOARD_WLAN_DEVICE                := bcmdhd
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_NVRAM_PATH_PARAM     := "/sys/module/dhd/parameters/nvram_path"
-WIFI_DRIVER_NVRAM_PATH           := "/system/etc/wifi/nvram_net.txt"
-WIFI_BAND                        := 802_11_ABG
-
-# CMHW
-BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
-
-# Force the screenshot path to CPU consumer (fix glitches)
-COMMON_GLOBAL_CFLAGS += -DFORCE_SCREENSHOT_CPU_PATH
-
 # SELinux
-BOARD_SEPOLICY_DIRS := \
-    device/samsung/n1awifi/sepolicy
+BOARD_SEPOLICY_DIRS += device/samsung/exynos5420-common/sepolicy
+
+# Inherit from exynos5420-common
+include device/samsung/exynos5420-common/BoardConfigCommon.mk
