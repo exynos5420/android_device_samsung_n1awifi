42ee8cbfafa99ea547048de72413328a23e97610
mosimchah <mosimchah@gmail.com>
2016-12-11 13:17:35 -0500

    Revert "device.mk: Build Gello"
    This reverts commit 2cd6dd5b281e1bd2cd001bca419e0b5393b30d52.
    
    Change-Id: Ibdb92104d88f6fac865e4dfe7d9a2dd52c2326e0

--- device.mk
+++ device.mk
@@ -55,10 +55,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
 
-# Gello (CM browser)
-PRODUCT_PACKAGES += \
-    Gello
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.target.rc \
