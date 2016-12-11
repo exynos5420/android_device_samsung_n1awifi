9ff009d36640809025c99cb4e5e5de4caab88c0c
mosimchah <mosimchah@gmail.com>
2016-12-11 13:56:24 -0500

    A liquidy death
    Change-Id: Ib31b901fee15974d59186b140cd80b1ffb3e6218

--- ld.mk
+++ ld.mk
@@ -4,10 +4,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from n1awifi device
 $(call inherit-product, device/samsung/n1awifi/device.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common ld stuff.
+$(call inherit-product, vendor/ld/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_n1awifi
+PRODUCT_NAME := ld_n1awifi
 PRODUCT_DEVICE := n1awifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
