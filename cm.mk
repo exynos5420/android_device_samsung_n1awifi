f6814a241a631ddc4af05f0cc1f178b76a087ccb
Thomas Patry <thompatry@gmail.com>
2017-01-06 15:53:01 -0500

    n1awifi:Fixes OK Google.
    I hate this damn bug. I would have never thought to look here.
    
    Basically changes the order of the inherit are called.
    
    Change-Id: I2040dd192956267d3c8554246215d537c26dc491
    (cherry picked from commit fc13c1fa3e595d4664e7ab01168cef4008308c7a)

--- cm.mk
+++ cm.mk
@@ -4,6 +4,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from n1awifi device
 $(call inherit-product, device/samsung/n1awifi/device.mk)
 
+PRODUCT_NAME := full_n1awifi
+PRODUCT_DEVICE := n1awifi
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-P600
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
