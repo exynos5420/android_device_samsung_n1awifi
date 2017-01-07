6678cfafacd5ae35c329099c09193ff804417567
Thomas Patry <thompatry@gmail.com>
2017-01-07 15:20:02 -0600

    Clean up lineage.mk
    Change-Id: I6d85425b3dab43b14ab2894fa5d8f2eef91a234d

--- lineage.mk
+++ lineage.mk
@@ -4,11 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from n1awifi device
 $(call inherit-product, device/samsung/n1awifi/device.mk)
 
-PRODUCT_NAME := full_n1awifi
-PRODUCT_DEVICE := n1awifi
 PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := SM-P600
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
