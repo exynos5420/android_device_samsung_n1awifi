8cdadcd03beceaf3eb2b4fe57b8cc7b66ff4201e
zakooz <marcotribosklol@gmail.com>
2017-03-05 02:17:12 +0000

    lineage.mk: This is a samsung device
    PRODUCT_MANUFACTURER is used multiple times in build/core and is written to build.prop (used to be 'unknown').
    
    Change-Id: I0949460487e7ac949f90baa475d8467b99f17b10

--- lineage.mk
+++ lineage.mk
@@ -11,9 +11,13 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_NAME := lineage_n1awifi
 PRODUCT_DEVICE := n1awifi
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-P600 \
     PRODUCT_NAME=n1awifi \
     PRODUCT_DEVICE=n1awifi \
-    TARGET_DEVICE=n1awifi
+    TARGET_DEVICE=n1awifi \
+    BUILD_FINGERPRINT="samsung/lt03wifiue/lt03wifiue:5.1.1/LMY47X/P600UEUDOK1:user/release-keys" \
+    PRIVATE_BUILD_DESC="lt03wifiue-user 5.1.1 LMY47X P600UEUDOK1 release-keys"
